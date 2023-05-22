76cf9acc6c02c3fd8dee963cf76f22c033ea9bbc
Marcus Davanço <md.daves@gmail.com>
2023-05-22 15:06:28 -0300

    feat: Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
+import { Router } from "./Router"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <h1>Hello World</h1>
+			<BrowserRouter>
+				<GlobalStyle />
+				<Router />
+			</BrowserRouter>
     </ThemeProvider>
   );
 }
